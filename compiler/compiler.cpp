42549345ebc21f39accbb74617e45c4efa9dc8bf
Michael Schaffer <mls278@case.edu>
2015-11-10 00:07:57 -0500

    Fix compiler, it works now

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -10,50 +10,133 @@
 
 using namespace std;
 
+/* Implementation note: wire naming convention
+ *
+ * All wire names must be unique. To ensure uniqueness, their names are a
+ * combination of the block number and pin name of the pin that drives it,
+ * as follows:
+ *
+ * wire_<blocknumber>_<pinname>
+ */
+
 string generatePicCode(FlowChart flow)
 {
-  string includes;
-  string mainFile = "int main() {\n";
-  std::set<int> expanded;
-  std::deque<int> toBeExpanded;
+  unordered_set<string> blockNames;
+  string mainFile = "int main() {\n\n";
+  unordered_set<int> expanded;
+  deque<int> toBeExpanded;
+  unordered_set<string> wires;
+  // This loop does 3 things:
+  // - Populate the blockNames set
+  // - Populate the wires set
+  // - Identify input blocks (blocks with no input connections)
   for(auto element : flow.blocks)
   { 
     int i = element.first;
     Block block =  element.second;
+    // If this is an input block, make sure it's expanded first
     if(block.inputConnections.empty())
     {
       toBeExpanded.push_back(i);
     }
-    std::size_t already = includes.find(block.blockType.name.toStdString()+".h");
-    if(already == std::string::npos){
-        includes += "#include \"" + block.blockType.name.toStdString() + ".h\"\n";
+    // make sure this block's name is in the blockNames set
+    blockNames.insert(block.blockType.name.toStdString());
+    // for each output connection
+    for (auto element : block.outputConnections) {
+        string pinName = element.first;
+        string wireName = "wire_";
+        wireName += to_string(i);
+        wireName += "_";
+        wireName += pinName;
+        wires.insert(wireName);
     }
   }
-  includes += "\n";
+  // directed graph traversal: write the sequence of functions
   while(!toBeExpanded.empty())
   {
     int blockIndex = toBeExpanded.front();
     toBeExpanded.pop_front();
-    Block block = flow.blocks[blockIndex];
-    bool found_all = isExpandable(block, expanded);
-    if(found_all)
+    // if this block has not been expanded yet
+    if (expanded.find(blockIndex) == expanded.end())
     {
-       auto results = expand(blockIndex, flow, toBeExpanded, expanded, mainFile);
-       mainFile = std::get<0>(results);
-       toBeExpanded = std::get<1>(results);
-       expanded = std::get<2>(results);
-    }
-    else
-    {
-       toBeExpanded.push_back(blockIndex);
+        Block block = flow.blocks[blockIndex];
+        // check if all this block's inputs have been expanded
+        bool found_all = isExpandable(block, expanded);
+        if(found_all)
+        {
+            // mark this node as expanded
+            expanded.insert(blockIndex);
+            // add all of this node's outputs to the front of the queue
+            for(auto output : block.outputConnections)
+            {
+               for(auto element : output.second){
+                  int outIndex = element.first;
+                  if (expanded.count(outIndex) <= 0) {
+                    toBeExpanded.push_front(outIndex);
+                  }
+               }
+            }
+            // write this node's function call
+            vector<BlockPin> wireBlockPins;
+            for (QString inputPinNameQ : block.blockType.inputs.keys()) {
+                string inputPinName = inputPinNameQ.toStdString();
+                if (!block.inputIsConnected(inputPinName)) {
+                    // error: bad structure
+                    return "";
+                } else {
+                    BlockPin wireSource = block.inputConnections[inputPinName];
+                    wireBlockPins.push_back(wireSource);
+                }
+            }
+            for (QString outputPinNameQ : block.blockType.outputs.keys()) {
+                string outputPinName = outputPinNameQ.toStdString();
+                BlockPin outputBlockPin(blockIndex, outputPinName);
+                wireBlockPins.push_back(outputBlockPin);
+            }
+            string funcCall = block.blockType.name.toStdString();
+            funcCall += "(";
+            for (int i = 0; i < wireBlockPins.size(); i++) {
+                BlockPin wireSource = wireBlockPins[i];
+                funcCall += "&wire_";
+                funcCall += to_string(wireSource.first);
+                funcCall += "_";
+                funcCall += wireSource.second;
+                if (i < wireBlockPins.size() - 1) {
+                    funcCall += ",";
+                }
+            }
+            funcCall += ");";
+            mainFile += funcCall;
+            mainFile += "\n";
+        }
+        else
+        {
+           toBeExpanded.push_back(blockIndex);
+        }
     }
   }
-  mainFile += "}";
-  includes += mainFile;
-  return includes;
+  mainFile += "\n}";
+
+  string includesFile = "";
+  for (string blockName : blockNames) {
+      includesFile += "#include \"";
+      includesFile += blockName;
+      includesFile += "\"\n";
+  }
+  includesFile += "\n";
+
+  string declarationsFile = "";
+  for (string wireName : wires) {
+      declarationsFile += "int ";
+      declarationsFile += wireName;
+      declarationsFile += ";\n";
+  }
+  declarationsFile += "\n";
+
+  return includesFile + declarationsFile + mainFile;
 }
 
-bool isExpandable(Block block, std::set<int> expanded)
+bool isExpandable(Block block, std::unordered_set<int> expanded)
 {
     bool found_all = true;
     for(auto input : block.inputConnections)
@@ -67,41 +150,3 @@ bool isExpandable(Block block, std::set<int> expanded)
     }
     return found_all;
 }
-
-std::tuple<std::string, std::deque<int>, std::set<int>> expand(int blockIndex, FlowChart flow, std::deque<int> toBeExpanded, std::set<int> expanded, std::string mainFile)
-{
-   Block block = flow.blocks[blockIndex];
-   for(auto output : block.outputConnections){
-        mainFile += "int wire_" + output.first + "=0;\n";
-   }
-   string type = block.blockType.name.toStdString();
-   std::transform(type.begin(), type.end(), type.begin(), ::tolower);
-   mainFile += type + "(";
-   expanded.insert(blockIndex);
-   for(auto input : block.inputConnections){
-       mainFile += "*wire_" + input.first + ",";
-   }
-   for(auto output : block.outputConnections)
-   {
-         mainFile += "*wire_"+ output.first + ",";
-   }
-   mainFile.pop_back();
-   mainFile += ");\n";
-   for(auto output : block.outputConnections)
-   {
-      for(auto element : output.second){
-         int outIndex = element.first;
-         Block out = flow.blocks[outIndex];
-
-         if(isExpandable(out,expanded))
-         {
-            toBeExpanded.push_back(outIndex);
-            expand(outIndex, flow, toBeExpanded, expanded, mainFile);
-         }
-      }
-   }
-   auto results = std::make_tuple(mainFile, toBeExpanded, expanded);
-   return results;
-}
-
-
--- compiler/compiler.h
+++ compiler/compiler.h
@@ -9,8 +9,8 @@
 #include <set>
 
 std::string generatePicCode(FlowChart flow);
-bool isExpandable(Block block, std::set<int> toBeExpanded);
-std::tuple<std::string, std::deque<int>, std::set<int>> expand(int blockIndex, FlowChart flow, std::deque<int> toBeExpanded, std::set<int> expanded, std::string mainFile);
+bool isExpandable(Block block, std::unordered_set<int> toBeExpanded);
+std::tuple<std::string, std::deque<int>, std::unordered_set<int>> expand(int blockIndex, FlowChart flow, std::deque<int> toBeExpanded, std::unordered_set<int> expanded, std::string mainFile);
 
 #endif // COMPILER_H
 
