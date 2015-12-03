2027f4a48f3623848c7ca8261bfa7d6c77c23bb4
Michael Schaffer <mls278@case.edu>
2015-12-03 01:22:45 -0500

    Document BlockOptionComboBox

--- flowchart/blockoptioncombobox.h
+++ flowchart/blockoptioncombobox.h
@@ -6,10 +6,28 @@
 #include "gui/blockoptioncontrol.h"
 #include <QMap>
 
+/**
+ * A BlockOptionComboBox is a BlockOption that specifies a set of values, from
+ * which one is chosen.
+ */
 class BlockOptionComboBox : public BlockOption {
 public:
+    /**
+     * Construct a BlockOptionComboBox.
+     * @param displayName the display name
+     * @param defaultValue the default value
+     * @param choices an ordered mapping from each choice's display name to
+     * its value
+     */
     explicit BlockOptionComboBox(QString displayName, QString defaultValue, QMap<QString, QString> choices);
+
+    /**
+     * Create the BlockOptionControl for controlling this option.
+     * The returned object will be a BlockOptionControlComboBox.
+     * @return A pointer to the heap-allocated control object.
+     */
     virtual BlockOptionControl *makeControl() const override;
+
 private:
     QMap<QString, QString> m_choices;
 };
