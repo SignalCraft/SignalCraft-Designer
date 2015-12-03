e7c8ea36593a4570b38550aca322d0c0da0578ac
Michael Schaffer <mls278@case.edu>
2015-12-03 01:23:59 -0500

    Document DataType

--- flowchart/datatype.h
+++ flowchart/datatype.h
@@ -1,6 +1,9 @@
 #ifndef DATATYPE
 #define DATATYPE
 
+/**
+ * The DataType enum represents the type of data carried by a wire.
+ */
 enum DataType { ANALOG = 1, DIGITAL = 2, ANALOG_BUS = 3, DIGITAL_BUS = 4 };
 
 #endif // DATATYPE
