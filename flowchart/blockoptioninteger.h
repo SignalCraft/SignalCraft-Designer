8dbcc5efeae20dd4bf6d12abb98c610bbdc56bfc
Michael Schaffer <mls278@case.edu>
2015-12-03 01:23:03 -0500

    Document BlockOptionInteger

--- flowchart/blockoptioninteger.h
+++ flowchart/blockoptioninteger.h
@@ -5,10 +5,27 @@
 #include <QString>
 #include "gui/blockoptioncontrol.h"
 
+/**
+ * A BlockOptionInteger is a BlockOption that selects from a range of integers.
+ */
 class BlockOptionInteger : public BlockOption {
 public:
+    /**
+     * Construct a BlockOptionInteger.
+     * @param displayName the display name
+     * @param defaultValue the default value
+     * @param minimum the minimum value
+     * @param maximum the maximum value
+     */
     explicit BlockOptionInteger(QString displayName, QString defaultValue, int minimum, int maximum);
+
+    /**
+     * Create the BlockOptionControl for controlling this option.
+     * The returned object will be a BlockOptionControlInteger.
+     * @return A pointer to the heap-allocated control object.
+     */
     virtual BlockOptionControl *makeControl() const override;
+
 private:
     int m_minimum;
     int m_maximum;
