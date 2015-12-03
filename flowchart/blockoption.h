74cc54f9b5848b91d8572061e5ae8f9659691415
Michael Schaffer <mls278@case.edu>
2015-12-03 01:02:57 -0500

    Document BlockOption

--- flowchart/blockoption.h
+++ flowchart/blockoption.h
@@ -4,14 +4,56 @@
 #include <QString>
 #include "gui/blockoptioncontrol.h"
 
+/**
+ * The immutable BlockOption abstract class represents one of a
+ * blocktype's options.
+ *
+ * A BlockOption stores all the data about the option's type, possible
+ * values, default value, and display characteristics. It also handles the
+ * creation of the appropriate BlockOptionControl object.
+ *
+ * Since BlockOption is abstract, only its subclasses should be
+ * instantiated. Subclasses must override makeControl() but should not
+ * override makeControl(QString), which calls makeControl internally.
+ */
 class BlockOption {
 public:
+    /**
+     * Construct an invalid BlockOption.
+     */
     explicit BlockOption();
+
+    /**
+     * Construct a BlockOption.
+     * @param displayName the BlockOption's display name.
+     * @param defaultValue the BlockOption's default value.
+     */
     explicit BlockOption(QString displayName, QString defaultValue);
+
+    /**
+     * @return the display name
+     */
     QString displayName() const;
+
+    /**
+     * @return the default value
+     */
     QString defaultValue() const;
+
+    /**
+     * Create the BlockOptionControl for controlling this option.
+     * This is a pure virtual function: subclasses must implement it.
+     * @return A pointer to the heap-allocated control object.
+     */
     virtual BlockOptionControl *makeControl() const;
+
+    /**
+     * Create the BlockOptionControl for controlling this option, with the given current value.
+     * This function calls makeControl internally.
+     * @return A pointer to the heap-allocated control object.
+     */
     BlockOptionControl *makeControl(QString currentValue) const;
+
 private:
     QString m_displayName;
     QString m_defaultValue;
