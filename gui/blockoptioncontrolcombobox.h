59d2c7910af7ae74ce463de3d7b72c1058108dd2
Michael Schaffer <mls278@case.edu>
2015-12-04 01:30:58 -0500

    Document BlockOptionControlComboBox

--- gui/blockoptioncontrolcombobox.h
+++ gui/blockoptioncontrolcombobox.h
@@ -7,11 +7,24 @@
 #include <QComboBox>
 #include <QMap>
 
+/**
+ * A BlockOptionControlComboBox is a BlockOptionControl that allows the user to
+ * choose one of several possible values.
+ */
 class BlockOptionControlComboBox : public BlockOptionControl {
 public:
+    /**
+     * Construct a BlockOptionControlComboBox.
+     * @param title the control's title
+     * @param choices an ordered mapping from choice names to choice values
+     * @param parent the optional parent widget
+     */
     explicit BlockOptionControlComboBox(const QString & title, QMap<QString, QString> choices, QWidget *parent = 0);
+
     virtual void setCurrentValue(QString value) override;
+
     virtual QString textValue() const override;
+
 private:
     QComboBox *m_control;
 };
