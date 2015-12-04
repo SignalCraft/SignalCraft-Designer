c5884cb24e5ed5e64ee5843382146554cc2ca88a
Michael Schaffer <mls278@case.edu>
2015-12-04 00:59:16 -0500

    Document PinGraphicsItem

--- gui/pingraphicsitem.h
+++ gui/pingraphicsitem.h
@@ -5,16 +5,40 @@
 #include <QString>
 #include "flowchart/datatype.h"
 
-class PinGraphicsItem : public QGraphicsItem
-{
+/**
+ * @brief The PinGraphicsItem class is a QGraphicsItem that represents a block's pin.
+ */
+class PinGraphicsItem : public QGraphicsItem {
 public:
+    /**
+     * Construct a PinGraphicsItem.
+     * @param parent the BlockGraphicsItem that this pin belongs to
+     * @param pinName the pin's name
+     * @param pinDataType the pin's data type
+     * @param isOutput true if the pin is an output, false if it's an input
+     */
     PinGraphicsItem(QGraphicsItem *parent, QString pinName, DataType pinDataType, bool isOutput);
+
     void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0) override;
+
     QRectF boundingRect() const override;
+
+    /**
+     * @return the pin's name
+     */
     QString pinName();
+
+    /**
+     * @return the pin's data type
+     */
     DataType pinDataType();
+
+    /**
+     * @return true if this pin is an output, false if it's an input
+     */
     bool isOutput();
-protected:
+
+private:
     QString m_pinName;
     DataType m_pinDataType;
     bool m_isOutput;
