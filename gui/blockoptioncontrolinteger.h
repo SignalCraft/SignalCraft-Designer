7acd6958e140028e7e1f74a674e5323bef9d1ad8
Michael Schaffer <mls278@case.edu>
2015-12-04 01:31:22 -0500

    Document BlockOptionControlInteger

--- gui/blockoptioncontrolinteger.h
+++ gui/blockoptioncontrolinteger.h
@@ -6,11 +6,25 @@
 #include <QWidget>
 #include <QSpinBox>
 
+/**
+ * A BlockOptionControlInteger is a BlockOptionControl that allows the user to
+ * specify an integer within a certain range.
+ */
 class BlockOptionControlInteger : public BlockOptionControl {
 public:
+    /**
+     * Construct a BlockOptionControlInteger.
+     * @param title the control's title
+     * @param minimum the minimum value
+     * @param maximum the maximum value
+     * @param parent the optional parent widget
+     */
     explicit BlockOptionControlInteger(const QString & title, int minimum, int maximum, QWidget *parent = 0);
+
     virtual void setCurrentValue(QString value) override;
+
     virtual QString textValue() const override;
+
 private:
     QSpinBox *m_control;
 };
