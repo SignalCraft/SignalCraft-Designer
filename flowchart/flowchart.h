6571b9a70e482eaede107cf15df173fdba42a64f
Michael Schaffer <mls278@case.edu>
2015-12-03 21:43:27 -0500

    Document FlowChart

--- flowchart/flowchart.h
+++ flowchart/flowchart.h
@@ -7,17 +7,78 @@
 #include "flowchart/block.h"
 #include "flowchart/blocktype.h"
 
+/**
+ * The FlowChart class represents a user-written program as a set of
+ * interconnected blocks.
+ *
+ * A FlowChart contains a number of blocks, each identified by a non-negative
+ * index. Though connection information is stored by the block, the handling
+ * of requests to connect arbritrary pins is handeled by FlowChart.
+ */
 class FlowChart {
 public:
+    /**
+     * Construct an empty FlowChart.
+     */
     FlowChart();
+
+    /**
+     * Construct a FlowChart with the given mapping from indexes to blocks.
+     * @param blocks a mapping from indexes to blocks
+     */
     FlowChart(QHash<int, Block> blocks);
+
+    /**
+     * Add a new block to this FlowChart.
+     * @param blockType the type of the new block
+     * @param pos the position of the new block
+     * @return the new block's index
+     */
     int addBlock(BlockType blockType, QPointF pos);
+
+    /**
+     * Connect two BlockPins to each other.
+     * This method only suceedes if one of the pins is an input and the other is an output.
+     * @param sourceBlockID the first BlockPin's block index
+     * @param sourcePinName the first BlockPin's pin name
+     * @param sinkBlockID the second BlockPin's block index
+     * @param sinkPinName the second BlockPin's pin name
+     */
     void connect(int sourceBlockID, QString sourcePinName, int sinkBlockID, QString sinkPinName);
+
+    /**
+     * Move a block to the given position.
+     * @param blockIndex the index of the block to move
+     * @param pos the block's new position
+     */
     void moveBlock(int blockIndex, QPointF pos);
+
+    /**
+     * Set a block's mapping of option names to values.
+     * @param blockIndex the index of the block to manipulate
+     * @param optionValues the complete mapping of option names to values
+     */
     void setBlockOptionValues(int blockIndex, QHash<QString, QString> optionValues);
+
+    /**
+     * Get a block's position.
+     * @param blockIndex the index of the block
+     * @return the block's position
+     */
     QPointF blockPos(int blockIndex) const;
+
+    /**
+     * @return a mapping of block indexes to Block objects
+     */
     QHash<int, Block> blocks() const;
+
+    /**
+     * Get the block with the given index
+     * @param blockIndex the block's index
+     * @return the block
+     */
     Block block(int blockIndex) const;
+
 private:
     QHash<int, Block> m_blocks;
     int m_currentIndex = 0;
