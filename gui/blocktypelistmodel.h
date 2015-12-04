5c363e7687b271992aba56707cd61a137cf07428
Michael Schaffer <mls278@case.edu>
2015-12-04 01:37:41 -0500

    Document BlockTypeListModel

--- gui/blocktypelistmodel.h
+++ gui/blocktypelistmodel.h
@@ -11,15 +11,40 @@
 
 class QMimeData;
 
+/**
+ * The BlockTypeListModel class is a QAbstractListModel that wraps the list of
+ * block types in a data model suitable for displaying with a list widget.
+ */
 class BlockTypeListModel : public QAbstractListModel {
 public:
+    /**
+     * Construct a BlockTypeListModel with no block types.
+     * @param parent the optional parent widget
+     */
     BlockTypeListModel(QObject *parent = 0);
+
+    /**
+     * Construct a BlockTypeListModel.
+     * @param blockTypes the list of BlockTypes
+     * @param parent the optional parent widget
+     */
     BlockTypeListModel(QList<BlockType> blockTypes, QObject *parent = 0);
+
+    /**
+     * Get the BlockType at the given index.
+     * @param index the index to look at
+     * @return the block type
+     */
     BlockType blockTypeAt(const QModelIndex &index) const;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
-    QMimeData *mimeData(const QModelIndexList &indexes) const override;
+
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+    virtual QMimeData *mimeData(const QModelIndexList &indexes) const override;
+
 private:
     QList<BlockType> m_blockTypes;
 };
