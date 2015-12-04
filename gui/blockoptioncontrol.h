0e9b650d3f64767b07876b082fb8fa44492ef4c2
Michael Schaffer <mls278@case.edu>
2015-12-04 01:23:08 -0500

    Document BlockOptionControl

--- gui/blockoptioncontrol.h
+++ gui/blockoptioncontrol.h
@@ -5,11 +5,33 @@
 #include <QString>
 #include <QWidget>
 
+/**
+ * A BlockOptionControl is an input widget used in an BlockOptionsDialog
+ * to set one of a block's options.
+ *
+ * BlockOptionControl must be subclassed to be used, and all subclasses
+ * must implement setCurrentValue(QString) and textValue().
+ */
 class BlockOptionControl : public QGroupBox {
     Q_OBJECT
 public:
+    /**
+     * Construct a BlockOptionControl.
+     * @param title the control's title
+     * @param parent the optional parent widget
+     */
     explicit BlockOptionControl(const QString & title, QWidget *parent = 0);
+
+    /**
+     * Set the current value of the control.
+     * @param value the new value
+     */
     virtual void setCurrentValue(QString value);
+
+    /**
+     * Determine the current value of the control.
+     * @return the current value
+     */
     virtual QString textValue() const;
 };
 
