7e190cfb91fb3168bdf133e0342f8d01733917fc
Michael Schaffer <mls278@case.edu>
2015-12-04 00:42:33 -0500

    Document ApplicationData

--- gui/applicationdata.h
+++ gui/applicationdata.h
@@ -5,9 +5,20 @@
 #include <QString>
 #include "flowchart/blocktype.h"
 
+/**
+ * The ApplicationData class stores all the information that is loaded at
+ * runtime and must persist for the lifetime of the process.
+ */
 class ApplicationData {
 public:
+    /**
+     * Construct an ApplicationData object.
+     */
     ApplicationData();
+
+    /**
+     * Stores an order mapping from block names to BlockTypes.
+     */
     QMap<QString, BlockType> blockTypes;
 };
 
