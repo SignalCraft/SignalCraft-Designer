729f5b268f9948256ab34a80042d6d5acea3d8c1
Michael Schaffer <mls278@case.edu>
2015-12-04 01:17:10 -0500

    Document BlockOptionsDialog

--- gui/blockoptionsdialog.h
+++ gui/blockoptionsdialog.h
@@ -11,15 +11,31 @@ namespace Ui {
     class BlockOptionsDialog;
 }
 
+/**
+ * A BlockOptionsDialog is a dialog box for setting a block's options.
+ */
 class BlockOptionsDialog : public QDialog {
     Q_OBJECT
 public:
+    /**
+     * Construct a BlockOptionsDialog.
+     * @param bt the type of block to get options for
+     * @param optionValues the mapping of option names to their current values
+     * @param parent the optional parent widget
+     */
     explicit BlockOptionsDialog(BlockType bt, QHash<QString, QString> optionValues, QWidget *parent = 0);
+
     ~BlockOptionsDialog();
+
+    /**
+     * @return the mapping of option names to the values set in this dialog
+     */
     QHash<QString, QString> optionValues() const;
+
 protected slots:
     void accept();
     void reject();
+
 private:
     Ui::BlockOptionsDialog *ui;
     QHash<QString, BlockOptionControl*> m_optionControls;
