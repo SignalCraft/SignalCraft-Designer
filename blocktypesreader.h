4cb790eeaadc370c078c4a77acf393034d0e8820
Michael Schaffer <mls278@case.edu>
2015-12-04 01:42:52 -0500

    Document BlockTypesReader

--- blocktypesreader.h
+++ blocktypesreader.h
@@ -11,12 +11,30 @@
 #include "flowchart/blockoption.h"
 #include <QSharedPointer>
 
+/**
+ * A BlockTypesReader reads a JSON file from an IO device as a list of
+ * serialized BlockTypes file, and stores an ordered mapping from block type
+ * names to the BlockType objects.
+ */
 class BlockTypesReader {
 public:
+    /**
+     * Construct a BlockTypesReader.
+     */
     BlockTypesReader();
-    QMap<QString, BlockType> blockTypes();
+
+    /**
+     * @return an ordered mapping from block type names to BlockTypes.
+     */
+    QMap<QString, BlockType> blockTypes() const;
+
+    /**
+     * Read a JSON file as a list of block types to be stored.
+     * @param device the device to read from
+     */
     void read(QIODevice *device);
-protected:
+
+private:
     QMap<QString, BlockType> m_blockTypes;
     QMap<QString, BlockType> readBlockTypes(QJsonValue node);
     BlockType readBlockType(QJsonValue node);
