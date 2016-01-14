e80052108a25ada406ddcebd23cab3f0c6b23e44
Michael Schaffer <mls278@case.edu>
2016-01-14 11:04:22 -0500

    Change data types

--- flowchart/datatype.h
+++ flowchart/datatype.h
@@ -6,7 +6,7 @@
 /**
  * The DataType enum represents the type of data carried by a wire.
  */
-enum DataType { ANALOG = 1, DIGITAL = 2, ANALOG_BUS = 3, DIGITAL_BUS = 4 };
+enum DataType { FLOAT = 1, BOOL = 2, INTEGER = 3, FLOAT_ARRAY = 4, BOOL_ARRAY = 5, INTEGER_ARRAY = 6 };
 
 QJsonValue DataType_toJson(DataType obj);
 DataType DataType_fromJson(QJsonValue node);
